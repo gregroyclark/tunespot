51b1528346e227f500bef2dcef8ebbba6ffd0305
gregroyclark <gregroyclark@gmail.com>
2021-03-23 17:26:54 -0500

    stubbed function component, created AUTH_URL string, imported Container from react-bootstrap

---
+++ client/src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from 'react-bootstrap'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=66fc5169a39843ad81b3cb835da6a3b5&response_type=code&redirect_uri=http://localhost3000&scope=streaming%20user-read-email%20user-read-private%20user-modify-playback-state"
+
+export default function Login() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
