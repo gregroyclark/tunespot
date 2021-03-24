b4678f688dc53f1d07753f3bf2c51897198da9f3
gregroyclark <gregroyclark@gmail.com>
2021-03-24 14:34:15 -0500

    server: fixed typo ln4, added refresh post; useAuth: added token promise; added .gitignore

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -10,9 +10,20 @@ const useAuth = (code) => {
         axios.post('http://localhost:3001/login', {
             code,
         }).then(res => {
-            console.log(res.data)
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({}, null, '/')
+        }).catch(() => {
+            window.location = '/'
         })
     }, [code])
+
+    useEffect(() => {
+
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
 
 export default useAuth;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const spotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "https://localhost:3000",
+        clientId: "66fc5169a39843ad81b3cb835da6a3b5",
+        clientSecret: "8d2833617b6447cd99f107053bad6e0b",
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken().then(
+        (data) => {
+          console.log('The access token has been refreshed!');
+      
+          spotifyApi.setAccessToken(data.body['access_token']);
+        },
+        function(err) {
+          console.log('Could not refresh access token', err);
+        }
+      );
+})
 
 app.post('/login', (req, res) => {
     const code = req.body.code;
@@ -24,7 +45,8 @@ app.post('/login', (req, res) => {
                 expiresIn: data.body.expires_in
         })
     })
-    .catch(() => {
+    .catch((err) => {
+        console.log(err)
         res.sendStatus(400)
     })
 })
