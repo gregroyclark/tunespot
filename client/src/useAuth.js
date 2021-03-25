2f02956c9c61b6aa7ed4f094ad22a14f4f7a453a
gregroyclark <gregroyclark@gmail.com>
2021-03-25 14:06:29 -0500

    formatted useAuth file

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios'
 
 const useAuth = (code) => {
@@ -7,31 +7,36 @@ const useAuth = (code) => {
     const [expiresIn, setExpiresIn] = useState()
 
     useEffect(() => {
-        axios.post('http://localhost:3001/login', {
-            code,
-        }).then(res => {
-            setAccessToken(res.data.accessToken)
-            setRefreshToken(res.data.refreshToken)
-            setExpiresIn(res.data.expiresIn)
-            window.history.pushState({}, null, '/')
-        }).catch(() => {
-            window.location = '/'
-        })
+        axios
+            .post('http://localhost:3001/login', {
+                code,
+            }).then(res => {
+                setAccessToken(res.data.accessToken)
+                setRefreshToken(res.data.refreshToken)
+                setExpiresIn(res.data.expiresIn)
+                window.history.pushState({}, null, '/')
+            })
+            .catch(() => {
+                window.location = '/'
+            })
     }, [code])
 
     useEffect(() => {
         if (!refreshToken || !expiresIn) return
         const interval = setInterval(() => {
 
-            axios.post('http://localhost:3001/refresh', {
-                refreshToken,
-            }).then(res => {
-                setAccessToken(res.data.accessToken)
-                setExpiresIn(res.data.expiresIn)
-                window.history.pushState({}, null, '/')
-            }).catch(() => {
-                window.location = '/'
-            })
+            axios
+                .post('http://localhost:3001/refresh', {
+                    refreshToken,
+                })
+                .then(res => {
+                    setAccessToken(res.data.accessToken)
+                    setExpiresIn(res.data.expiresIn)
+                    window.history.pushState({}, null, '/')
+                })
+                .catch(() => {
+                    window.location = '/'
+                })
         }, (expiresIn - 60) * 1000)
         
         return () => clearInterval(interval)
