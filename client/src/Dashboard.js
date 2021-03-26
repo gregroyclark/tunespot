30343282ced105ed84c576c238c09a6f9748d606
gregroyclark <gregroyclark@gmail.com>
2021-03-26 16:07:29 -0500

    minor formatting

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,10 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useAuth from './useAuth'
 import { Container, Form } from 'react-bootstrap'
+import SpotifyWebApi from 'spotify-web-api-node'
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: "66fc5169a39843ad81b3cb835da6a3b5",
+})
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
     const [search, setSearch] = useState("")
+    const [searchResults, setSearchResults] = useState([])
+
+    useEffect(() => {
+        if (!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    }, [accessToken])
+
+    useEffect(() => {
+        if (!searchResults) return setSearchResults([])
+        if (!accessToken) return
+
+        spotifyApi.searchTracks(search).then(res => {
+            setSearchResults(res.body.tracks.items.map(track => {
+
+                const smallestAlbumImage = track.album.images.reduce(
+                    (smallest, image) => {
+                        if (image.height < smallest.height) return image
+                        return smallest
+                    },
+                    track.album.images[0]
+                )
+
+                return {
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.url
+                }
+            }))
+        })
+
+    }, [search, accessToken])
+
     return (
         <Container 
             className="d-flex flex-column py-2" 
--- server/server.js
+++ server/server.js
@@ -9,11 +9,12 @@ app.use(bodyParser.json())
 
 app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
+    console.log(refreshToken)
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: "https://localhost:3000",
+        redirectUri: "http://localhost:3000",
         clientId: "66fc5169a39843ad81b3cb835da6a3b5",
         clientSecret: "8d2833617b6447cd99f107053bad6e0b",
-        refreshToken
+        refreshToken,
     })
 
     spotifyApi
@@ -33,7 +34,7 @@ app.post('/refresh', (req, res) => {
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: "https://localhost:3000",
+        redirectUri: "http://localhost:3000",
         clientId: "66fc5169a39843ad81b3cb835da6a3b5",
         clientSecret: "8d2833617b6447cd99f107053bad6e0b",
     })
@@ -44,7 +45,7 @@ app.post('/login', (req, res) => {
             res.json({
                 accessToken: data.body.access_token,
                 refreshToken: data.body.refresh_token,
-                expiresIn: data.body.expires_in
+                expiresIn: data.body.expires_in,
         })
     })
     .catch((err) => {
