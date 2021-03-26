c57d1dec3419ccb7da3e892d563e0d3c10d7fced
gregroyclark <gregroyclark@gmail.com>
2021-03-26 16:09:42 -0500

    imported TrackSearchResult, added searchResults to useEffect dependency

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import useAuth from './useAuth'
+import TrackSearchResult from './TrackSearchResult'
 import { Container, Form } from 'react-bootstrap'
 import SpotifyWebApi from 'spotify-web-api-node'
 
@@ -21,12 +22,14 @@ export default function Dashboard({ code }) {
         if (!searchResults) return setSearchResults([])
         if (!accessToken) return
 
+        let cancel = false
         spotifyApi.searchTracks(search).then(res => {
+            if (cancel) return
             setSearchResults(res.body.tracks.items.map(track => {
 
                 const smallestAlbumImage = track.album.images.reduce(
                     (smallest, image) => {
-                        if (image.height < smallest.height) return image
+                        if (image.height < smallest.height) return image 
                         return smallest
                     },
                     track.album.images[0]
@@ -36,12 +39,14 @@ export default function Dashboard({ code }) {
                     artist: track.artists[0].name,
                     title: track.name,
                     uri: track.uri,
-                    albumUrl: smallestAlbumImage.url
+                    albumUrl: smallestAlbumImage.url,
                 }
-            }))
+            })
+          )
         })
 
-    }, [search, accessToken])
+        return () => cancel = true
+    }, [search, accessToken, searchResults])
 
     return (
         <Container 
@@ -55,7 +60,9 @@ export default function Dashboard({ code }) {
                 onChange={e => setSearch(e.target.value)}
             />
             <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-                Songs
+                {searchResults.map(track => (
+                    <TrackSearchResult track={track} key={track.uri} />
+                ))}
             </div>
             <div>Bottom</div>
         </Container>
