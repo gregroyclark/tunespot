f367bad8c8f4ac7934cdd7f5bc019c7dd96e0cb5
gregroyclark <gregroyclark@gmail.com>
2021-03-26 19:20:13 -0500

    fixed minor typos

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.post('/refresh', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    const code = req.body.code;
+    const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
         redirectUri: process.env.REDIRECT_URI,
         clientId: process.env.CLIENT_ID,
